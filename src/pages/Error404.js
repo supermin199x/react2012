5c2b30f8dab0d307de2f02e3e615b31145d357b0
supermin199x <s.statiion@icloud.com>
2021-03-18 10:37:21 +0700

    fix

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -4,9 +4,8 @@ import '../pages/Error404.css';
 const Error404 = () => {
     return (
         <div className="error404">
-            <div>
-                <h1>Page not found 404</h1>
-            </div>
+            <h1 className="error">404</h1>
+            <h3 className="notfound">File not found.</h3>
         </div>
     );
 } 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,6 @@ const Home = () => {
 		<div className="home">
 			<Navbar />
 			<Content />
-			
 		</div>
 	);
 };
